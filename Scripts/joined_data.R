621559c04aa8225521045f9550524019e69e4202
Selen Gizlice <selengizlice@mac.mynetworksettings.com>
2024-12-06 22:14:58 -0500

    updating program to try to get make to work

--- Scripts/joined_data.R
+++ Scripts/joined_data.R
@@ -6,7 +6,7 @@ flights <- read_csv("source_data/flights.csv")
 airline <- airlines %>% rename(AIRLINE=IATA_CODE, LONGNAME=AIRLINE)
 
 status <- flights %>%
-  filter(!is.na(ARRIVAL_DELAY)) %>%
+  filter(complete.cases(ARRIVAL_DELAY)) %>%
   mutate(delay = ifelse(ARRIVAL_DELAY > 0, 'Delayed', 'On Time'))
 
 joined_status <- status %>% inner_join(airline, by="AIRLINE")
